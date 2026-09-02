3fdc4888be25014d8caeb3b367c8a4d90715e9b6
ipurnias-ux <ipurnias@gmail.com>
2026-09-02 16:54:40 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:sizer/sizer.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:go_router/go_router.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SharedPreferences.getInstance();
-  runApp(const MyApp());
+void main(){runApp(const MyApp());}
+class MyApp extends StatelessWidget{
+const MyApp({super.key});
+@override
+Widget build(BuildContext context){
+return MaterialApp(
+debugShowCheckedModeBanner:false,
+home:Scaffold(
+appBar:AppBar(title:Text('Instalite')),
+body:Center(child:Text('OK'))
+)
+);
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Sizer(
-      builder: (c, o, d) {
-        return MaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            textTheme: GoogleFonts.poppinsTextTheme(),
-          ),
-          routerConfig: GoRouter(
-            routes: [
-              GoRoute(
-                path: '/',
-                builder: (context, state) => Scaffold(
-                  appBar: AppBar(title: Text('Instalite 1.0.1+2', style: GoogleFonts.poppins())),
-                  body: Center(
-                    child: Text('Instalite OK - Connected Lahat!', style: TextStyle(fontSize: 18.sp)),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
 }
